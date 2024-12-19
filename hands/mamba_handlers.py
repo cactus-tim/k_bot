00d341df69ff6e4167da959b900c10f0ec98aa87
Соснин Тимофей Алексеевич <tasosnin@edu.hse.ru>
2024-12-19 20:13:47 +0300

    small fix

--- hands/mamba_handlers.py
+++ hands/mamba_handlers.py
@@ -65,10 +65,11 @@ class ChromeExtended(webdriver.Chrome):
     def __init__(self, *args, options=None, proxy=None, **kwargs):
         options = options or Options()
 
-        options.add_argument("--headless=new")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1200,875")
 
         if proxy:
             context = tempfile.TemporaryDirectory()
